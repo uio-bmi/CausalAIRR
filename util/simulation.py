5e10bf5947eabadfa84ec47ae673b4a2841b6026
ghadialhajj <ghadi.alhajj8@gmail.com>
2021-11-09 22:25:41 +0100

    Added exp3, scenario3

--- util/simulation.py
+++ util/simulation.py
@@ -21,22 +21,39 @@ def get_exp_protocol(hospital: str) -> Signal:
     return make_exp_protocol_signal(protocol_id=protocol_id, signal_name="experimental_protocol")
 
 
+def get_selection(hospital: str, state: bool) -> bool:
+    if hospital == "hospital1":
+        if state:
+            return bool(np.random.binomial(1, 0.8))
+        else:
+            return bool(np.random.binomial(1, 0.2))
+    if hospital == "hospital2":
+        if state:
+            return bool(np.random.binomial(1, 0.2))
+        else:
+            return bool(np.random.binomial(1, 0.8))
+
+
 def get_repertoire(immune_state: bool, experimental_protocol: Signal, path: Path, sequence_count: int,
                    immune_state_signal: Signal, seed: int, repertoire_implanting_rate: float) -> str:
     PathBuilder.build(path)
 
     # make OLGA repertoire from the default OLGA TCRB model
-    repertoire = make_olga_repertoire(path=path, sequence_count=sequence_count, seed=seed, confounder=True, confounder_name="olga_model")
+    repertoire = make_olga_repertoire(path=path, sequence_count=sequence_count, seed=seed, confounder=True,
+                                      confounder_name="olga_model")
 
     # implant a signal in the repertoire based on the immune state
     if immune_state:
-        repertoire = make_repertoire_with_signal(repertoire=repertoire, signal=immune_state_signal, result_path=path / "immuneML_with_signal/",
+        repertoire = make_repertoire_with_signal(repertoire=repertoire, signal=immune_state_signal,
+                                                 result_path=path / "immuneML_with_signal/",
                                                  repertoire_implanting_rate=repertoire_implanting_rate)
     else:
-        repertoire = make_repertoire_without_signal(repertoire=repertoire, signal_name=immune_state_signal.id, result_path=path / "immuneML_with_signal/")
+        repertoire = make_repertoire_without_signal(repertoire=repertoire, signal_name=immune_state_signal.id,
+                                                    result_path=path / "immuneML_with_signal/")
 
     # simulate experimental protocol signal in the repertoire
-    repertoire = make_repertoire_with_signal(repertoire=repertoire, signal=experimental_protocol, result_path=path / "immuneML_with_protocol/",
+    repertoire = make_repertoire_with_signal(repertoire=repertoire, signal=experimental_protocol,
+                                             result_path=path / "immuneML_with_protocol/",
                                              repertoire_implanting_rate=repertoire_implanting_rate)
 
     return repertoire.data_filename
