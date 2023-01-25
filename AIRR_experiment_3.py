e594981be3a6b15e82984df5fafb02701110a70e
pavlovicmilena <milenapavlovic775@gmail.com>
2023-01-25 17:09:04 +0100

    update batch_corrections for the run

--- AIRR_experiment_3.py
+++ AIRR_experiment_3.py
@@ -2,7 +2,7 @@ import argparse
 from datetime import datetime
 from pathlib import Path
 
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, LinearRegression
 
 from causal_airr_scripts.dataset_util import setup_path
 from causal_airr_scripts.experiment3.SimConfig import SimConfig, ImplantingConfig, ImplantingSetting, ImplantingUnit, ImplantingGroup
@@ -11,7 +11,7 @@ from causal_airr_scripts.util import write_config
 
 
 def main(namespace):
-    run_one_config({'control': ImplantingUnit(0.4, 0.25, 0.875, 0), "name": 'testing',
+    run_one_config({'control': ImplantingUnit(0.4, 0.25, 0.875, 0), "name": 'full_run',
                     'batch0': ImplantingUnit(0.16, 3 / 84, 7 / 16, 0.25),
                     'batch1': ImplantingUnit(0.64, 3 / 4, 63 / 64, 0.75),
                     'batch_test': ImplantingUnit(0.4, 0.25, 0.875, batch_implanting_prob=0.5)},
@@ -26,7 +26,7 @@ def run_one_config(proba_set, sequence_count, result_path, num_processes):
                         signal_name='batch_signal')
 
     config = SimConfig(k=3, repetitions=5, olga_model_name='humanTRB', sequence_encoding='continuous_kmer', signal=signal,
-                       batch_corrections=[None, Ridge(alpha=1e8), Ridge(alpha=1e9)], batch_signal=batch_signal,
+                       batch_corrections=[None, LinearRegression(), Ridge(alpha=1e6), Ridge(alpha=1e9)], batch_signal=batch_signal,
                        implanting_config=ImplantingConfig(
                            control=ImplantingSetting(
                                train=ImplantingGroup(baseline=proba_set['control'], modified=proba_set['control'], seq_count=sequence_count),
